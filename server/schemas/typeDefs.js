ccc4804a948c7f105d473daef9af8493761d3128
Dom231 <114959233+Dom231@users.noreply.github.com>
2023-03-21 17:28:11 -0500

    def&res

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,34 @@
 const { gql } = require('apollo-server-express')
 
 const typeDefs = gql `
+type User{
+    _id: ID!
+    userName: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+}
+
+type Book{
+    authors : String
+    description : String
+    bookId : String!
+    image: String
+    link: String
+    title: String!
+}
+
 type Query{ 
+    user(id: ID!): [User]
+    books : [Book]
+
 
 }
 
 type Mutation {
+    addUser(userName: String!, email: String!, password: String!): User
+    addBook(bookId: String!,title: String!): Book
+    deleteBook(bookId: String!, title: String!)
     
 }
 `
